c473d1797c5a9a2d89c026e1b563794f22ee52bf
YoungLee2004 <144761712+YoungLee2004@users.noreply.github.com>
2026-01-08 16:14:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Stage from "./pages/Stage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/stage/:id" element={<Stage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/stage/:id" element={<Stage />} />
+    </Routes>
   );
 }
